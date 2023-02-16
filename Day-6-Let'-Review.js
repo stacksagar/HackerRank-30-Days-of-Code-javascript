57dbcbdf9ead21c5f63b1076411c450871429eb4
stacksagar <stacksagar@gmail.com>
2023-02-16 15:00:28 +0600

    day 6

--- Day-6-Let'-Review.js
+++ Day-6-Let'-Review.js
@@ -4,6 +4,20 @@ Hacker
 Rank
 `);
 
-function processData(input) {
-  console.log("input ", input);
+function processData(input = "") {
+  const all_string = input.split("\n").filter((s) => s);
+
+  all_string.forEach((string) => {
+    let even = "";
+    let odd = "";
+    for (let i = 0; i < string.length; i++) {
+      if (i % 2 === 0) {
+        even += string[i];
+      } else {
+        odd += string[i];
+      }
+    }
+
+    console.log(`${even} ${odd}`);
+  });
 }
